0934e26677a28959fdc46dd8a33224994a3a93b2
hugolalmeida <alm22005@byui.edu>
2022-10-28 16:56:46 -0300

    lesson 7 discover page

--- chamber/scripts/assign.js
+++ chamber/scripts/assign.js
@@ -22,3 +22,58 @@ if (wday === 1){
 else if(wday ===2){
     document.getElementById("banner").textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 }
+
+let imagesToLoad = document.querySelectorAll(".holder-img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+}else{
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+const daysDisplay = document.querySelector(".days")
+const visitsDisplay = document.querySelector(".visits");
+const dayStr = document.querySelector(".day");
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let numDay = 1;
+if (d.setDate(d.getDate() + 1)){
+    window.localStorage.clear();
+}  
+if (numDay === 1){
+dayStr.textContent ="day" 
+}else{
+dayStr.textContent ="days"  
+}
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits + 1;
+    
+} else{
+    visitsDisplay.textContent = numVisits + 1;
+    numDay++;
+}
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
+
+daysDisplay.textContent = numDay;
