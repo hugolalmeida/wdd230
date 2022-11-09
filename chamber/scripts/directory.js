d0f223c6e03d16f8001b2b5d977a9d0906582656
hugolalmeida <alm22005@byui.edu>
2022-11-09 11:31:47 -0300

    directory page and data json incomplete

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,26 @@
+function toggleMenu () {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+let d = new Date();
+
+let year = document.querySelector(".currentyear").textContent = d.getFullYear();
+
+let last_update = document.querySelector("#jscript").textContent = document.lastModified;
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(d);
+
+let current_date = document.getElementById("current-day").textContent = fulldateUK;
+
+        
+    
+let wday = d.getDay();
+if (wday === 1){
+    document.getElementById("banner").textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
+else if(wday ===2){
+    document.getElementById("banner").textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
+
+document.getElementById("date").value = fulldateUK;
