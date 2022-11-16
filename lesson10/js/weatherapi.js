258af299094e73c89d7a57ede604d003d77c782d
hugolalmeida <alm22005@byui.edu>
2022-11-16 13:39:45 -0300

    round number

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -22,8 +22,7 @@ async function apiFetch() {
   apiFetch();
 
 function displayResults(weatherData) {
-    // currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp}</strong>`;
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
   const desc = weatherData.weather[0].description;
 
