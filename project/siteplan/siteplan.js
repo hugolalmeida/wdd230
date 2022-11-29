25b348b60089c2b6b4d1fd18460f4077778a6c5a
hugolalmeida <alm22005@byui.edu>
2022-11-29 17:14:30 -0300

    bountful foods siteplan

---
+++ project/siteplan/siteplan.js
@@ -0,0 +1,5 @@
+let d = new Date();
+
+let year = document.querySelector(".currentyear").textContent = d.getFullYear();
+
+let last_update = document.querySelector("#jscript").textContent = document.lastModified;
