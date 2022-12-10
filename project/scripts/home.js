2272f69fc45377236e415bfa53ecca84d318529b
hugolalmeida <hugogule@gmail.com>
2022-12-10 19:23:10 -0300

    home page missing only news

--- project/scripts/home.js
+++ project/scripts/home.js
@@ -5,6 +5,18 @@ const captionDesc = document.querySelector('#icon-name');
 const url = "https://api.openweathermap.org/data/2.5/forecast?lat=32.4207&lon=-104.2288&units=imperial&appid=f4342634d1d9263d2c7801dc80233d6c";
 const placeName = document.querySelector('#city-name');
 const request = document.querySelector("#num-request");
+const humidity = document.querySelector("#humidity");
+const weatherIcon1 = document.querySelector('.icon1');
+const weatherIcon2 = document.querySelector('.icon2');
+const weatherIcon3 = document.querySelector('.icon3');
+const day1 = document.querySelector(".icon-name1");
+const day2 = document.querySelector(".icon-name2");
+const day3 = document.querySelector(".icon-name3");
+const d1 =document.querySelector("#data1");
+const d2 =document.querySelector("#data2");
+const d3 =document.querySelector("#data3");
+const c_date = document.querySelector("#city-date");
+
 
 async function apiFetch() {
     try {
@@ -24,14 +36,40 @@ async function apiFetch() {
 
 function displayResults(weatherData) {
     let temperature = weatherData.list[0].main.temp; //farh
+    const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+      dateStyle: "full"
+    }).format(d);
+    c_date.textContent = fulldateUK;
     currentTemp.textContent = temperature.toFixed(0);
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.list[0].weather[0].icon}.png`;
+    
+    const iconsrc1 = `https://openweathermap.org/img/w/${weatherData.list[4].weather[0].icon}.png`;
+    const iconsrc2 = `https://openweathermap.org/img/w/${weatherData.list[11].weather[0].icon}.png`;
+    const iconsrc3= `https://openweathermap.org/img/w/${weatherData.list[21].weather[0].icon}.png`;
     const desc = weatherData.list[0].weather[0].description;
+    
+    const desc1 = weatherData.list[4].main.temp;
+    const desc2 = weatherData.list[11].main.temp;
+    const desc3 = weatherData.list[21].main.temp;
 
-    weatherIcon.setAttribute('src', iconsrc);
+d1.textContent = weatherData.list[4].dt_txt;
+d2.textContent = weatherData.list[11].dt_txt;
+d3.textContent = weatherData.list[21].dt_txt;
+
+  weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
+  weatherIcon1.setAttribute('src', iconsrc1);
+  weatherIcon1.setAttribute('alt', desc1);
+  weatherIcon2.setAttribute('src', iconsrc2);
+  weatherIcon2.setAttribute('alt', desc2);
+  weatherIcon3.setAttribute('src', iconsrc3);
+  weatherIcon3.setAttribute('alt', desc3);
   captionDesc.textContent = desc;
+  day1.textContent = desc1;
+  day2.textContent = desc2;
+  day3.textContent = desc3;
   placeName.textContent = weatherData.city.name;
+  humidity.textContent = weatherData.list[0].main.humidity
 }
 let numRequest = Number(window.localStorage.getItem("request-ls"));
       numRequest++;
